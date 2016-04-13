6d37bca9a93ef4dd1cee47416b55bc5040452985
liyong <liyong@addnewer.com>
2016-04-13 12:11:03 +0800

    删除模板编译文件

--- db.inc.php
+++ db.inc.php
@@ -3,7 +3,7 @@ return array(
 	//数据库配置
 	'DB_HOST'=>'127.0.0.1',//主机
 	'DB_USER'=>'root',//帐号
-	'DB_PASSWORD'=>'123456',//密码
+	'DB_PASSWORD'=>'',//密码
 	'DB_NAME'=>'blog',//数据库
 	'DB_CHARSET'=>'UTF8',//字符集
 );
